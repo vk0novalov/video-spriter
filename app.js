2053879eef967a42416ebcc3b1b07181868add9d
Victor Konovalov <victor@konovalov.pw>
2022-04-27 23:48:34 +0300

    Clean up and refactoring

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 'use strict';
 
-require('./lib/requirements')();
+const { check: checkRequirements } = require('./lib/requirements');
+
+// check system dependencies
+const requiredCommands = [
+  { name: 'ffmpeg', package: 'ffmpeg' },
+  { name: 'convert', package: 'imagemagick' },
+];
+if (checkRequirements(requiredCommands) === false) {
+  process.exit(1);
+}
+
 require('./lib/config')();
 
 const videoStorage = require('./lib/video-storage');
@@ -9,7 +19,7 @@ const { SpriteCreator } = require('./lib/sprite-creator');
 
 async function createSpriteFromVideo(videoKey, previewKey, previewIntervalMs) {
   console.log('Preparing to generate thumbnails with sprite from video');
-  const spriteCreator = new SpriteCreator();
+  const spriteCreator = new SpriteCreator(previewIntervalMs);
   spriteCreator.on('frame', (frame) =>
     console.log(`Frame #${frame} was successfully created`)
   );
@@ -19,7 +29,7 @@ async function createSpriteFromVideo(videoKey, previewKey, previewIntervalMs) {
   const uploadStream = videoStorage.getUploadStream(previewKey, (err) => {
     console.log(err ? console.error('❌ Error', err) : '✅ Done');
   });
-  await spriteCreator.process(downloadStream, uploadStream, previewIntervalMs);
+  await spriteCreator.process(downloadStream, uploadStream);
 }
 
 createSpriteFromVideo(
